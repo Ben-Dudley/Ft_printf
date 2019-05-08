473e787f84ec4e77feed540905c2d78e6b144483
Ben-Dudley <vilenaguslyakova@gmail.com>
2019-05-08 18:22:12 +0300

    Ft_printf without flags in double

---
+++ Makefile
@@ -0,0 +1,47 @@
+# **************************************************************************** #
+#                                                                              #
+#                                                         :::      ::::::::    #
+#    Makefile                                           :+:      :+:    :+:    #
+#                                                     +:+ +:+         +:+      #
+#    By: bdudley <marvin@42.fr>                     +#+  +:+       +#+         #
+#                                                 +#+#+#+#+#+   +#+            #
+#    Created: 2018/11/26 17:59:10 by bdudley           #+#    #+#              #
+#    Updated: 2019/04/30 16:12:26 by jgoyette         ###   ########.fr        #
+#                                                                              #
+# **************************************************************************** #
+
+#NAME = ft_printf
+NAME = libftprintf.a
+FLAGS = #-Wall -Wextra -Werror
+SRCS  =  src/ft_printf.c src/ft_handle_format.c src/ft_parse_optionals.c \
+	src/utils/ft_cast_nums.c src/utils/ft_num_help.c src/utils/ft_print_help.c \
+	src/print_funcs/ft_print_char.c src/print_funcs/ft_print_str.c \
+	src/print_funcs/ft_print_pointer.c \
+	src/print_funcs/ft_print_binary.c src/print_funcs/ft_print_integer.c \
+	src/print_funcs/ft_print_hex.c src/print_funcs/ft_print_octal.c \
+	src/print_funcs/ft_print_double.c src/print_funcs/ft_print_unsigned.c \
+	src/utils/ft_putcolor.c
+OBJS = $(SRCS:.c=.o)
+LIB = ./lib/libft/libft.a
+
+all: $(NAME)
+
+$(NAME): $(OBJS)
+	make -C ./lib/libft
+#	gcc $(FLAGS) $(OBJS) $(LIB) -o $(NAME)
+	cp lib/libft/libft.a ./$(NAME)
+	ar rc $(NAME) $(OBJS)
+	ranlib $(NAME)
+
+%.o: %.c
+	gcc $(FLAGS) -c $< -o $@
+
+clean:
+	make -C ./lib/libft clean
+	rm -f $(OBJS)
+
+fclean: clean
+	make -C ./lib/libft fclean
+	rm -f $(NAME)
+
+re: fclean all
---
+++ src/print_funcs/ft_print_double.c
@@ -0,0 +1,76 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_double.c                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: bdudley <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/03/29 17:53:01 by bdudley           #+#    #+#             */
+/*   Updated: 2019/04/26 16:27:50 by jgoyette         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../../inc/ft_printf.h"
+
+static char	*ft_put_double(long double value, t_format *f)
+{
+	char	*str;
+	char	*integer;
+	char	*fractional;
+	int		counter;
+
+	integer = ft_itoa_base(value, 10, 'a'); //ch delat
+//	counter = ft_strlen(integer) + (value > 0) ? counter : counter + 1;
+//    counter = ((f->precision + counter) > f->min_width) ? f->precision + counter : f->min_width;
+	fractional = ft_strnew(f->precision + 1);
+	value = value > 0 ? value : -value;
+	value -= (int)value;
+	fractional[0] = '.';
+	counter = 1;
+	while (counter <= f->precision)
+	{
+		if (counter == f->precision && ((int)(value * 100) % 10) > 4)
+			value += 0.1;
+		fractional[counter] = (int)(value * 10) + '0';
+		value = value * 10 - (int)(value * 10);
+		counter++;
+	}
+	str = ft_strnew(ft_strlen(integer) + ft_strlen(fractional));
+	ft_strcpy(str, integer);
+	ft_strcpy(str + ft_strlen(integer), fractional);
+	free(integer);
+	free(fractional);
+	return (str);
+}
+
+void			ft_print_double(char **str, t_format *f)
+{
+	long double	value;
+	int		counter;
+	char	*ptr;
+	char    *s;
+
+	f->precision = (f->precision == 0) ? 6 :f->precision;
+	if (f->length == 7)
+	    value = va_arg(f->ap, long double);
+	else
+        value = va_arg(f->ap, double);
+	ptr = ft_put_double(value, f);
+    counter = ft_strlen(ptr);
+	if (f->precision == 6 && f->min_width != 0)
+    {
+	    s = ft_strnew(f->min_width - counter);
+	    s = (char *)ft_memset((void *)s, ' ', f->min_width - counter);
+        counter += ft_strlen(s);
+        ft_putbuffer(f, s, ft_strlen(s));
+	    free(s);
+    }
+	if (f->space == 1 && f->min_width == 0)
+    {
+        ft_putbuffer(f, " ", 1);
+        counter++;
+    }
+    ft_putbuffer(f, ptr, ft_strlen(ptr));
+	*str += 1;
+	f->printed += counter;
+}
